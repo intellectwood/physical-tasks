778cc032e7ea1ed2c9a5c158299b63a25fcc563e
intellectwood <30440402+intellectwood@users.noreply.github.com>
2017-08-13 00:41:44 +0800

    Create task8-12-spec.js

---
+++ physical-tasks/spec/task8-12-spec.js
@@ -0,0 +1,31 @@
+const main = require('../main/task8-12.js');
+
+
+
+describe('打印水仙花数', () => {
+
+
+
+
+
+
+    it('val= 3 则符合水仙花的三位数有：\n', () => {
+        const val1=3;
+        const result = main (val1);
+        console.log('val= 3 则符合水仙花的三位数有：\n'+result+'\n');
+        expect(result).toEqual([ 1, 153, 370, 371, 407 ]);
+
+    })
+
+
+    it('val= 4 则符合水仙花的三位数有：\n', () => {
+        const val2=4;
+        const result = main (val2);
+        console.log('val= 4 则符合水仙花的数有：\n'+result+'\n');
+        expect(result).toEqual([ 1, 153, 370, 371, 407 ]);
+
+    })
+
+
+
+})
