0eb7831726d8ecbe4d8863731277d2d238c2e128
intellectwood <30440402+intellectwood@users.noreply.github.com>
2017-08-13 00:48:46 +0800

    Update task8-12-spec.js

--- physical-tasks/spec/task8-12-spec.js
+++ physical-tasks/spec/task8-12-spec.js
@@ -13,7 +13,7 @@ describe('打印水仙花数', () => {
         const val1=3;
         const result = main (val1);
         console.log('val= 3 则符合水仙花的三位数有：\n'+result+'\n');
-        expect(result).toEqual([ 1, 153, 370, 371, 407 ]);
+        expect(result).toEqual([153, 370, 371, 407 ]);
 
     })
 
@@ -22,7 +22,7 @@ describe('打印水仙花数', () => {
         const val2=4;
         const result = main (val2);
         console.log('val= 4 则符合水仙花的数有：\n'+result+'\n');
-        expect(result).toEqual([ 1, 153, 370, 371, 407 ]);
+        expect(result).toEqual([153, 370, 371, 407 ]);
 
     })
 
