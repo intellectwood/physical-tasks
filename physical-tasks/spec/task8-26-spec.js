ef2652919c36ebf4404ccddf5c51b187c8f31314
intellectwood <30440402+intellectwood@users.noreply.github.com>
2017-08-27 10:50:55 +0800

    Update task8-26-spec.js

--- physical-tasks/spec/task8-26-spec.js
+++ physical-tasks/spec/task8-26-spec.js
@@ -10,20 +10,14 @@ describe(
 
 
 
-    it(`给你一个整数n. 从 1 到 n 按照下面的规则打印每个数：
-如果这个数被3整除，打印fizz.
-如果这个数被5整除，打印buzz.
-如果这个数能同时被3和5整除，打印fizz buzz.：\n`, () => {
+    it(`给你一个整数10. 从 1 到 10 按照下面的规则打印每个数：\n`, () => {
       
 
         let ele=10;
 
         
         const result = main(ele);
-        console.log(`给你一个整数n. 从 1 到 n 按照下面的规则打印每个数：
-如果这个数被3整除，打印fizz.
-如果这个数被5整除，打印buzz.
-如果这个数能同时被3和5整除，打印fizz buzz.：\n`);     
+        console.log(`给你一个整数10. 从 1 到 10 按照下面的规则打印每个数：\n`);     
          console.log(result); 
         
         expect(result).toEqual('fizz buzz fizz fizz buzz');
