19b8cfb9ba26445b10f04384c64608b8ccd69bb4
intellectwood <30440402+intellectwood@users.noreply.github.com>
2017-08-27 10:47:08 +0800

    Create task8-26.js

---
+++ physical-tasks/main/task8-26.js
@@ -0,0 +1,21 @@
+function main(ele)
+{
+
+
+    let resultArr=[];
+    let result;
+    let times=1;
+    
+    while(times<=ele){
+        if(times%3==0) resultArr.push("fizz");  
+        if(times%5==0) resultArr.push("buzz");
+        
+        times++;
+    }
+    result=resultArr.join(" ");
+
+
+    return result;
+}
+module.exports = main;
+
